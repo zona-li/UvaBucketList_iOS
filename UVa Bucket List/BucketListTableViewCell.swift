4e366fcabecd7002c31a434826cf1eb291d126ec
Zona Clark <Zona@Zonas-MacBook.local>
2017-02-19 20:43:48 -0500

    Fixed previous commit

---
+++ UVa Bucket List/BucketListTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BucketListTableViewCell.swift
+//  UVa Bucket List
+//
+//  Created by Haoyang Li on 2/12/17.
+//  Copyright © 2017 Haoyang Li. All rights reserved.
+//
+
+import UIKit
+
+class BucketListTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var bucketListNameLabel: UILabel!
+    @IBOutlet weak var bucketListDateLabel: UILabel!
+    
+    
+    
+}
